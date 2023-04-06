d13d9976b8281a4ecb9305721c3696ca06629d3e
ilknur karaçelik <ilknurkaracelik@hotmail.com>
2023-04-06 09:38:18 +0300

    Model ve Viewbag çalışması

--- MvcLesson1/Controllers/HomeController.cs
+++ MvcLesson1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcLesson1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +12,31 @@ namespace MvcLesson1.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            //string[] kurslar = { "Mvc", "c#", "asp.net" };
+            List<Urun> urunler = new List<Urun>()
+            {
+                new Urun(){ UrunId=1,UrunAdi="Iphone",Aciklama="İyi",Fiyat=3000,Satistami=true},
+                new Urun(){ UrunId=2,UrunAdi="Iphone7",Aciklama="İyi",Fiyat=5000,Satistami=true},
+                new Urun(){ UrunId=3,UrunAdi="Iphone8",Aciklama="ehh",Fiyat=2000,Satistami=false},
+                new Urun(){ UrunId=4,UrunAdi="Iphone9",Aciklama="kötü",Fiyat=9000,Satistami=true}
+            };
+
+
+
+            List<Kategori> Kategoriler = new List<Kategori>()
+            {
+                new Kategori() { Id = 1, Ad = "Telefon" },
+                new Kategori() { Id = 2, Ad = "TV" },
+            };
+            ViewBag.UrunSayisi = urunler.Count;
+            ViewBag.Kategoriler = Kategoriler;
+
+            return View(urunler);
         }
+
+
+
+
         public ActionResult Services()
         {
             return View();
---
+++ MvcLesson1/Models/Kategori.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcLesson1.Models
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+    }
+}
